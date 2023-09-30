262b6f275f87bf160544487bc16127e904fbb043
LeslieGbikpi-Benissan <gbikpibenissal1127@students.bowiestate.edu>
2023-09-30 17:48:21 -0400

    Lab 2

---
+++ Lab2partb.py
@@ -0,0 +1,23 @@
+number1 = 10
+number2 = 11
+number3 = 12
+number4 = 13
+addition = number1 + number2 + number3 + number4
+multiplication = number1 * number2 * number3 * number4
+subtraction = number4 - number3 - number2 - number1
+division = (number4 / number3)
+print(addition, multiplication, subtraction, division)
+number1 = 13
+number2 = 12
+number3 = 11
+number4 = 10
+addition = number1 + number2 + number3 + number4
+multiplication = number1 * number2 * number3 * number4
+subtraction = number4 - number3 - number2 - number1
+division = (number4 / number3)
+print(addition,multiplication,subtraction,division)
+addition = number1 + number2
+multiplication = number3 * number4
+subtraction = number1 - number2
+division = (number1 / number2)
+print(addition,multiplication,subtraction,division)
