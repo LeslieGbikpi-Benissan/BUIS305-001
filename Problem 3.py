acc6e9e96d72013b0112ac3173488764f5f2c150
LeslieGbikpi-Benissan <gbikpibenissal1127@students.bowiestate.edu>
2023-11-03 22:33:42 -0400

    Midterm

---
+++ Problem 3.py
@@ -0,0 +1,8 @@
+sum_of_numbers = 100
+count = 50
+for i in range(5):
+    num = float(input("Enter a number: "))
+    sum_of_number = 100
+average = sum_of_numbers / 5
+print("sum of the numbers: ", sum_of_numbers)
+print("average of the numbers: ", average)
